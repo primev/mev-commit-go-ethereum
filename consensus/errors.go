1cc583c33dd34478d4beb206f64e8f75169c1a31
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-06-11 12:06:40 -0700

    Update errors.go

--- consensus/errors.go
+++ consensus/errors.go
@@ -29,7 +29,7 @@ var (
 
 	// ErrFutureBlock is returned when a block's timestamp is in the future according
 	// to the current node.
-	ErrFutureBlock = errors.New("block in the futuree")
+	ErrFutureBlock = errors.New("block in the future")
 
 	// ErrInvalidNumber is returned if a block's number doesn't equal its parent's
 	// plus one.
